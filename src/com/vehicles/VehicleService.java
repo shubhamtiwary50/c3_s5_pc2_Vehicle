fbd65eabed41ab91e585ba8308ad09181b9e81c7
SHUBHAM <shubhamtiwary50@gmail.com>
2022-08-23 19:25:37 +0530

    minor refactoring

--- src/com/vehicles/VehicleService.java
+++ src/com/vehicles/VehicleService.java
@@ -6,10 +6,9 @@ public class VehicleService {
         Bike bike = new Bike("H2R","YH2029","Sportsbike");
         Car car = new Car("BMW","BMW2022","Sedan");
 
-        System.out.println("The Bike type is " + bike.getVehicleType() +" its speed is " + bike.maxSpeed(bike.getVehicleType()));
-        System.out.println("ManufactureInformation = " + bike.getManufactureInformation());
-        System.out.println("Car type is " + car.getVehicleType() +" its speed is " + car.maxSpeed(car.getVehicleType()));
-        System.out.println("ManufactureInformation = " + car.getManufactureInformation());
+        System.out.println("The Bike type is : " + bike.getVehicleType() +" and its max speed is " + bike.maxSpeed(bike.getVehicleType()));
+        System.out.println("ManufactureInformation : " + bike.getManufactureInformation());
+        System.out.println("Car type is : " + car.getVehicleType() +" and its max speed is " + car.maxSpeed(car.getVehicleType()));
+        System.out.println("ManufactureInformation : " + car.getManufactureInformation());
     }
 }
-//changing remote
